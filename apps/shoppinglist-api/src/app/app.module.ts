40cdb2ea69a62cdc7707ba1d965b366fc4a3d1b1
Kneesal <nisalcottingham@gmail.com>
2023-08-20 12:02:11 +1200

    install typeorm

--- apps/shoppinglist-api/src/app/app.module.ts
+++ apps/shoppinglist-api/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common'
-
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'somehost',
+      port: 3306,
+      username: 'someuser',
+      password: 'somepw',
+      database: 'somedb',
+      entities: [],
+      synchronize: true
+    })
+    //, some modules
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
