94811aaa4bf4a7cd84e9cca92fe9dd29a183c5f0
Kneesal <nisalcottingham@gmail.com>
2023-08-21 19:08:56 +1200

    add users module

--- apps/shoppinglist-api/src/app/app.module.ts
+++ apps/shoppinglist-api/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { User } from '../modules/user/user.entity'
+import { UsersModule } from '../modules/user/user.module'
+import { DataSource } from 'typeorm'
 
 @Module({
   imports: [
@@ -12,12 +15,15 @@ import { AppService } from './app.service'
       username: 'someuser',
       password: 'somepw',
       database: 'somedb',
-      entities: [],
+      entities: [User],
       synchronize: true
-    })
+    }),
+    UsersModule
     //, some modules
   ],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ apps/shoppinglist-api/src/modules/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common'
+import { User } from './user.entity'
+import { UsersService } from './user.service'
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  findOne(@Body() id: string): Promise<User> {
+    return this.usersService.findOne(id)
+  }
+
+  @Post()
+  create(@Body() input: User): Promise<User> {
+    console.log(input)
+    return this.usersService.createUser(input)
+  }
+
+  @Delete('/remove')
+  remove(@Body() id: string): Promise<void> {
+    return this.usersService.remove(id)
+  }
+}
