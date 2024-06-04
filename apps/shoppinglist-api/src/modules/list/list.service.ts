3eb6f30d3ba5efdb25074bdabec7d432c49e877c
Kneesal <nisalcottingham@gmail.com>
2024-06-05 11:34:10 +1200

    fix: list service

---
+++ apps/shoppinglist-api/src/modules/list/list.service.ts
@@ -0,0 +1,4 @@
+import { List } from './list.entity'
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { DeleteResult, Repository } from 'typeorm'
